529cb5902533e8a01b87337b59cee9aca97d9cff
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2020-07-04 16:50:05 +0200

    solved with iterative DFS

--- graph-theory/Reachability.java
+++ graph-theory/Reachability.java
@@ -1,24 +1,34 @@
-package decomposition;
-
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.*;
 
 public class Reachability {
-    private static int reach(ArrayList<Integer>[] adj, int x, int y) {
-        //write your code here
+
+    private static int reach(Map<Integer, List<Integer>> adjacencyList, int x, int y) {
+
+        Stack<Integer> stack = new Stack<>();
+        boolean[] isVisited = new boolean[adjacencyList.size()];
+        stack.push(x);
+        //iterative DFS
+        while (!stack.isEmpty()) {
+            int current = stack.pop();
+            isVisited[current] = true;
+            if (current == y) return 1;
+
+            for (Integer vertex : adjacencyList.get(current)) {
+                if (!isVisited[vertex]) stack.push(vertex);
+            }
+        }
         return 0;
     }
 
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int m = scanner.nextInt();
-        ArrayList<Integer>[] adj = (ArrayList<Integer>[])new ArrayList[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new ArrayList<Integer>();
+        int V = scanner.nextInt();
+        int E = scanner.nextInt();
+        ArrayList<Integer>[] adj = (ArrayList<Integer>[]) new ArrayList[V];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new ArrayList<>();
         }
-        for (int i = 0; i < m; i++) {
+        for (int i = 0; i < E; i++) {
             int x, y;
             x = scanner.nextInt();
             y = scanner.nextInt();
@@ -27,7 +37,12 @@ public class Reachability {
         }
         int x = scanner.nextInt() - 1;
         int y = scanner.nextInt() - 1;
-        System.out.println(reach(adj, x, y));
+        Map<Integer, List<Integer>> adjacencyList = new HashMap<>();
+
+        for (int i = 0; i < adj.length; i++) {
+            adjacencyList.putIfAbsent(i, adj[i]);
+
+        }
+        System.out.println(reach(adjacencyList, x, y));
     }
 }
-
