a62c31332b67302922c5b1d13547bfdfb156f869
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2020-07-09 15:00:46 +0200

    Weighted quick-union by rank with path compression by halving

--- graph-theory/unionfind/UF.java
+++ graph-theory/unionfind/UF.java
@@ -1,25 +1,6 @@
 package unionfind;
 /******************************************************************************
- *  Compilation:  javac UF.java
- *  Execution:    java UF < input.txt
- *  Dependencies: StdIn.java StdOut.java
- *  Data files:   https://algs4.cs.princeton.edu/15uf/tinyUF.txt
- *                https://algs4.cs.princeton.edu/15uf/mediumUF.txt
- *                https://algs4.cs.princeton.edu/15uf/largeUF.txt
- *
  *  Weighted quick-union by rank with path compression by halving.
- *
- *  % java UF < tinyUF.txt
- *  4 3
- *  3 8
- *  6 5
- *  9 4
- *  2 1
- *  5 0
- *  7 2
- *  6 1
- *  2 components
- *
  ******************************************************************************/
 
 
@@ -27,59 +8,15 @@ import util.StdIn;
 import util.StdOut;
 
 /**
- * The {@code UF} class represents a <em>unionâ€“find data type</em>
- * (also known as the <em>disjoint-sets data type</em>).
- * It supports the classic <em>union</em> and <em>find</em> operations,
- * along with a <em>count</em> operation that returns the total number
- * of sets.
- * <p>
- * The unionâ€“find data type models a collection of sets containing
- * <em>n</em> elements, with each element in exactly one set.
- * The elements are named 0 through <em>n</em>â€“1.
- * Initially, there are <em>n</em> sets, with each element in its
- * own set. The <em>canonical element</em> of a set
- * (also known as the <em>root</em>, <em>identifier</em>,
- * <em>leader</em>, or <em>set representative</em>)
- * is one distinguished element in the set. Here is a summary of
- * he operations:
- * <ul>
- * <li><em>find</em>(<em>p</em>) returns the canonical element
- *     of the set containing <em>p</em>. The <em>find</em> operation
- *     returns the same value for two elements if and only if
- *     they are in the same set.
- * <li><em>union</em>(<em>p</em>, <em>q</em>) merges the set
- *     containing element <em>p</em> with the set containing
- *     element <em>q</em>. That is, if <em>p</em> and <em>q</em>
- *     are in different sets, replace these two sets
- *     with a new set that is the union of the two.
- * <li><em>count</em>() returns the number of sets.
- * </ul>
- * p>
- * The canonical element of a set can change only when the set
- * itself changes during a call to <em>union</em>&mdash;it cannot
- * change during a call to either <em>find</em> or <em>count</em>.
- * p>
- * This implementation uses <em>weighted quick union by rank</em>
- * with <em>path compression by halving</em>.
- * The constructor takes &Theta;(<em>n</em>) time, where
- * <em>n</em> is the number of elements.
- * The <em>union</em> and <em>find</em> operations take
- * &Theta;(log <em>n</em>) time in the worst case.
- * The <em>count</em> operation takes &Theta;(1) time.
- * Moreover, starting from an empty data structure with <em>n</em> sites,
- * any intermixed sequence of <em>m</em> <em>union</em> and <em>find</em>
- * operations takes <em>O</em>(<em>m</em> &alpha;(<em>n</em>)) time,
- * where &alpha;(<em>n</em>) is the inverse of
- * <a href = "https://en.wikipedia.org/wiki/Ackermann_function#Inverse">Ackermann's function</a>.
- * p>
- * For alternative implementations of the same API, see
- * {@link QuickUnionUF}, {@link QuickFindUF}, and {@link WeightedQuickUnionUF}.
- * For additional documentation, see
- * <a href="https://algs4.cs.princeton.edu/15uf">Section 1.5</a> of
- * <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
- * <p>
- * @author Robert Sedgewick
- * @author Kevin Wayne
+ * The constructor takes O(n) time, where  n is the number of elements.
+ * The union and find operations take O(log n) time in the worst case.
+ * The count operation takes O(1) time.
+ * Proposition. [Hopcroft-Ulman, Tarjan] Starting from an
+ * empty data structure, any intemixed sequence of M union-find ops
+ * on N objects makes ≤ c ( N + M lg* N ) array accesses.
+ * Analysis can be improved to N + M α(M, N).
+ * Simple algorithm with fascinating mathematics.
+ * https://en.wikipedia.org/wiki/Ackermann_function#Inverse
  */
 
 public class UF {
@@ -88,14 +25,6 @@ public class UF {
     private byte[] rank;   // rank[i] = rank of subtree rooted at i (never more than 31)
     private int count;     // number of components
 
-    /**
-     * Initializes an empty union-find data structure with
-     * {@code n} elements {@code 0} through {@code n-1}.
-     * Initially, each elements is in its own set.
-     *
-     * @param n the number of elements
-     * @throws IllegalArgumentException if {@code n < 0}
-     */
     public UF(int n) {
         if (n < 0) throw new IllegalArgumentException();
         count = n;
@@ -107,56 +36,24 @@ public class UF {
         }
     }
 
-    /**
-     * Returns the canonical element of the set containing element {@code p}.
-     *
-     * @param p an element
-     * @return the canonical element of the set containing {@code p}
-     * @throws IllegalArgumentException unless {@code 0 <= p < n}
-     */
     public int find(int p) {
         validate(p);
         while (p != parent[p]) {
-            parent[p] = parent[parent[p]];    // path compression by halving
+            parent[p] = parent[parent[p]];    // path compression by halving (grandparent intstead of parent)
             p = parent[p];
         }
         return p;
     }
 
-    /**
-     * Returns the number of sets.
-     *
-     * @return the number of sets (between {@code 1} and {@code n})
-     */
     public int count() {
         return count;
     }
 
-    /**
-     * Returns true if the two elements are in the same set.
-     *
-     * @param p one element
-     * @param q the other element
-     * @return {@code true} if {@code p} and {@code q} are in the same set;
-     * {@code false} otherwise
-     * @throws IllegalArgumentException unless
-     *                                  both {@code 0 <= p < n} and {@code 0 <= q < n}
-     * @deprecated Replace with two calls to {@link #find(int)}.
-     */
     @Deprecated
     public boolean connected(int p, int q) {
         return find(p) == find(q);
     }
 
-    /**
-     * Merges the set containing element {@code p} with the
-     * the set containing element {@code q}.
-     *
-     * @param p one element
-     * @param q the other element
-     * @throws IllegalArgumentException unless
-     *                                  both {@code 0 <= p < n} and {@code 0 <= q < n}
-     */
     public void union(int p, int q) {
         int rootP = find(p);
         int rootQ = find(q);
@@ -180,15 +77,6 @@ public class UF {
         }
     }
 
-    /**
-     * Reads an integer {@code n} and a sequence of pairs of integers
-     * (between {@code 0} and {@code n-1}) from standard input, where each integer
-     * in the pair represents some element;
-     * if the elements are in different sets, merge the two sets
-     * and print the pair to standard output.
-     *
-     * @param args the command-line arguments
-     */
     public static void main(String[] args) {
         int n = StdIn.readInt();
         UF uf = new UF(n);
