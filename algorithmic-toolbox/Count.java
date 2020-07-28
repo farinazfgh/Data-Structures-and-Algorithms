3062e2af127d6482589ab20d309b90fb2959e9e8
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2020-07-28 13:39:31 +0200

    suffix arrays

---
+++ algorithmic-toolbox/Count.java
@@ -0,0 +1,25 @@
+import util.StdIn;
+import util.StdOut;
+
+/******************************************************************************
+ Count Sort
+ */
+public class Count {
+
+    // Do not instantiate.
+    private Count() {
+    }
+
+    public static void main(String[] args) {
+        Alphabet alphabet = new Alphabet(args[0]);
+        final int R = alphabet.radix();
+        int[] count = new int[R];
+        while (StdIn.hasNextChar()) {
+            char c = StdIn.readChar();
+            if (alphabet.contains(c))
+                count[alphabet.toIndex(c)]++;
+        }
+        for (int c = 0; c < R; c++)
+            StdOut.println(alphabet.toChar(c) + " " + count[c]);
+    }
+}
