3716911a5692e7fd66b8332f11fe17fa488889f7
Farinaz Ghasemi <farinaz.ghasemi@gmail.com>
2020-08-06 15:44:10 +0200

    a simple phone book manager with HashMap

--- data-structures/PhoneBook.java
+++ data-structures/PhoneBook.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class PhoneBook {
 
     private FastScanner in = new FastScanner();
-    // Keep list of all existing (i.e. not deleted yet) contacts.
-    private List<Contact> contacts = new ArrayList<>();
+    private Map<Integer, String> contacts = new HashMap<>();
 
     public static void main(String[] args) {
         new PhoneBook().processQueries();
@@ -35,31 +32,14 @@ public class PhoneBook {
 
     private void processQuery(Query query) {
         if (query.type.equals("add")) {
-            // if we already have contact with such number,
-            // we should rewrite contact's name
-            boolean wasFound = false;
-            for (Contact contact : contacts)
-                if (contact.number == query.number) {
-                    contact.name = query.name;
-                    wasFound = true;
-                    break;
-                }
-            // otherwise, just add it
-            if (!wasFound)
-                contacts.add(new Contact(query.name, query.number));
+
+            contacts.put(query.number, query.name);
         } else if (query.type.equals("del")) {
-            for (Iterator<Contact> it = contacts.iterator(); it.hasNext(); )
-                if (it.next().number == query.number) {
-                    it.remove();
-                    break;
-                }
+            contacts.remove(query.number);
+
         } else {
-            String response = "not found";
-            for (Contact contact: contacts)
-                if (contact.number == query.number) {
-                    response = contact.name;
-                    break;
-                }
+            String response = contacts.get(query.number);
+            if (response == null) response = "not found";
             writeResponse(response);
         }
     }
